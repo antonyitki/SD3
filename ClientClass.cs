42e76f4ec2cf357f395d00e903a6646552da1355
Antony Itki <59201310+antonyitki@users.noreply.github.com>
2021-10-19 07:19:20 +0100

    Coursework_1, initial stage

---
+++ ClientClass.cs
@@ -0,0 +1,36 @@
+﻿namespace Coursework_1
+{
+    public class Client
+    {
+        public string FirstName { get; set; }
+        public string LastdName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public Client(string FirstName, string LastName,
+            string Email, string Adress, string PhoneNumber)
+        {
+            this.FirstName = FirstName;
+            this.LastdName = LastName;
+            this.Email = Email;
+            this.Address = Address;
+            this.PhoneNumber = PhoneNumber;
+        }
+        public Client()
+        {
+            FirstName = "Alex";
+            LastdName = "See";
+            Email = "global@gmail.com";
+            Address = "Froebel Colege";
+            PhoneNumber = "23456789";
+        }
+        public override string ToString()
+        {
+            return "FirstName: " + FirstName + "\n" +
+                "LastName: " + LastdName + "\n" +
+                "Email: " + Email + "\n" +
+                "Address: " + Address + "\n" +
+                "PhoneNumber: " + PhoneNumber;
+        }
+    }
+}
