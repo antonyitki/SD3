d03e79d117b7f8623ef814ef9a45c13ced0983bd
Antony Itki <59201310+antonyitki@users.noreply.github.com>
2021-10-23 22:51:22 +0100

    Add files via upload

---
+++ Exercise2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Cofe
+{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Enter numbers lenght: ");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            int input = 0;
+
+            for (int i = 0; i < lenght; ++i)
+            {
+                Console.WriteLine("Enter a number: ");
+                input = Convert.ToInt32(Console.ReadLine());
+
+                if (input >= 0)
+                {
+                    sum = sum + input;
+                }
+            }
+            Console.WriteLine("The sum of the positive numbers is: " + sum);
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+    class ServiceCharge {
+        //3 if statement
+
+
+    }
+
+
+    class tip{
+
+
+
+    }
+
+
+}
